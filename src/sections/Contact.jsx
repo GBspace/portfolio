af1902b62930b10cd670e4645846661da8ef41dc
Garima Jain <jain.garima88@gmail.com>
2025-02-22 23:10:28 +0800

    Update Contact.jsx

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -31,7 +31,7 @@ const Contact = () => {
           to_email: 'jain.garima88@gmail.com',
           message: form.message,
         },
-        import.meta.env.VITE_APP_EMAILJS,
+        import.meta.env.VITE_APP_EMAILJS_KEY,
       )
       .then(
         () => {
