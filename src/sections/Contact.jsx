4800fff044d4988d343634bb76791b4cbe4c869b
GBspace <jain.garima88@gmail.com>
2025-02-24 11:14:22 +0800

    update ref to env variables

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -22,8 +22,8 @@ const Contact = () => {
 
     emailjs
       .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        import.meta.env.VITE_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
           to_name: 'GB Jain',
@@ -31,7 +31,7 @@ const Contact = () => {
           to_email: 'jain.garima88@gmail.com',
           message: form.message,
         },
-        import.meta.env.VITE_APP_EMAILJS_KEY,
+        import.meta.env.VITE_EMAILJS_KEY,
       )
       .then(
         () => {
